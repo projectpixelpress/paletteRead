67e786d640124da6cba244a9afcbf45a86a07cf9
Bradley McCrorey <brad@projectpixelpress.com>
2017-04-07 10:23:57 -0500

    getting ready for npm publish

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * index.js
+ * Copyright (C) 2017 Bradley McCrorey <brad@projectpixelpress.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+const palette = require('./src');
+
+module.exports = Object.assign(palette);
